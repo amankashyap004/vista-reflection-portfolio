27bfb9d1c0ee34dcf57ca12f2a78d0f342e28c2b
amankashyap004 <amankashyap004@gmail.com>
2024-08-12 14:05:13 +0530

    create: footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <footer className="flex justify-center items-center w-full bg-[#545454] dark:bg-[#121212]">
+      <div className="container px-4 lg:px-10 py-2 lg:py-4 flex justify-center items-center w-full">
+        <p className="text-xs lg:text-xl text-white text-opacity-90">
+          © {new Date().getFullYear()}{" "}
+          <span className="text-[#FD6F00] font-semibold">Mumair</span> All
+          Rights Reserved, Inc.
+        </p>
+      </div>
+    </footer>
   );
 };
 
